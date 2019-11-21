36eb3da7cef5b1e6aca873d7d4a7d4f5e77e59eb
kingbabar8123 <54685065+kingbabar8123@users.noreply.github.com>
2019-11-20 18:23:09 -0600

    Update script.js
    added space to name

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
 //add your javascrip between these two lines of code
 
 document.getElementById('entrybutton').addEventListener('click', function(){
-	alert('Babar Kamran:' + document.getElementById('entryinput').value);
+	alert('Babar Kamran: ' + document.getElementById('entryinput').value);
 	document.getElementById('textoutput').innerHTML = document.getElementById('entryinput').value;
 });
 
